4d7178c57f98d1ba949b4d94edef64796994d21f
Freek Kalter <freek@kalteronline.org>
2016-01-30 23:41:06 +0100

    Switch to systemd for service management

--- apply.sh
+++ apply.sh
@@ -3,16 +3,17 @@
 cp ./iptables.sh /usr/local/sbin/iptables.sh
 chmod u=rwx,og= /usr/local/sbin/iptables.sh
 
-cp ./firewall.conf /etc/init/firewall.conf
-chmod u=rw,og=r /etc/init/firewall.conf
+cp ./iptables-sh.service /etc/systemd/system/iptables-sh.service
+chmod 664 /etc/systemd/system/iptables-sh.service
 
-sudo ./iptables.sh
+systemctl daemon-reload
+systemctl enable iptables-sh.service
 
 echo "Does ssh still work?"
 read -t 10 answer
 if [ "$answer" = "y" ] || [ "$answer" = "yes" ]; then
     exit
 else
-    sudo ./iptables.sh clear
+    systemctl disable iptables-sh.service
 fi
 
